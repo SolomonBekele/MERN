9bdf6ccbe172333f45935c5e17897dc253a107ef
SolomonBekele <solubman28@gmail.com>
2024-07-05 13:14:11 +0300

    done

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
 	cors: {
-		origin: ["https://chat-app-test-d8sx.onrender.com"],
+		origin: ["http://localhost:3000"],
 		methods: ["GET", "POST"],
 	},
 });
